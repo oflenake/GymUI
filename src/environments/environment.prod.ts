444f89feb0274672bf21d556cd8ec5042afd063c
Kgupi <Kgupi@users.noreply.github.com>
2019-05-15 22:01:59 +0200

    Fixed environment files and added maintenance image files.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'gymapp.vision-dream.local'
+  apiEndpoint: 'http://gymapp.vision-dream.local'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiEndpoint: 'localhost:54354'
+  apiEndpoint: 'http://localhost:54354'
 };
 
 /*
